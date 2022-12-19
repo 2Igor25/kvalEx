53eebff9b767c07b0b3f8dd3d70b99df6bc65392
tis <tis@rio-soft.ru>
2022-12-19 18:34:38 +0500

    Initial commit

---
+++ src/main/java/com/server/kval/KvalApplication.java
@@ -0,0 +1,12 @@
+package com.server.kval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KvalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/kval/KvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.kval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
